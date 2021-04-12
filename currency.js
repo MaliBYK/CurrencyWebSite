e2e1ecfba4718d9e1a3a1303c765a1a9a51dd77c
MaliBYK <developerbox00@gmail.com>
2021-04-12 11:51:46 +0300

    new api added

--- currency.js
+++ currency.js
@@ -1,11 +1,14 @@
 class Currency {
   static exchange(firstCurrency, secondCurrency, amount) {
     return new Promise(function (resolve, reject) {
-      fetch(`https://api.exchangeratesapi.io/latest?base=${firstCurrency}`)
+      fetch(
+        `http://api.currencylayer.com/live?access_key=dfe3da272d64e79c1486d48fce1bb176`
+      )
         .then(response => response.json())
         .then(data => {
-          let change = data.rates[secondCurrency];
-          resolve(amount * change);
+          let change = data.quotes[`USD${secondCurrency}`];
+          let change2 = data.quotes[`USD${firstCurrency}`];
+          resolve((amount * change) / change2);
         })
         .catch(err => reject(new Error(err)));
     });
